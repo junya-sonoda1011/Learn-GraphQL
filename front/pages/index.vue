555c0bd4cebd624c202820ffe73782e602f99697
jpnm-panda <jpnm0415shkm@gmail.com>
2023-01-24 09:51:44 +0900

    @nuxtjs/apolloの設定

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,52 +1,49 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
-    </div>
-  </section>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          ユーザー一覧
+        </v-card-title>
+        <v-card-text>
+          <v-simple-table>
+            <thead>
+              <tr>
+                <th>ユーザー名</th>
+                <th>仕事</th>
+                <th>趣味</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="user in users" :key="user.id">
+                <th>{{ user.name }}</th>
+                <th>{{ user.work }}</th>
+                <th>{{ user.hobby }}</th>
+              </tr>
+            </tbody>
+          </v-simple-table>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import Card from '~/components/Card'
+// import 'vue-apollo';
+import usersQuery from '../apollo/queries/users.gql';
 
 export default {
   name: 'IndexPage',
-  components: {
-    Card
-  }
-}
+  data() {
+    return {
+      users: [],
+    };
+  },
+  apollo: {
+    users: {
+      prefetch: true,
+      query: usersQuery,
+    },
+  },
+};
 </script>
